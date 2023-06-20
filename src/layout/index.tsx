99f5e1d1ea59e60f92a19c006cb1c09b23035ac0
fmaulll <maul2821@gmail.com>
2023-06-20 16:46:44 +0700

    layouts

---
+++ src/layout/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC, useContext } from "react";
+import { PokemonContext } from "../context/PokemonContext";
+import ModalLoader from "../components/ModalLoader";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout: FC<Props> = ({ children }) => {
+  const { loading } = useContext(PokemonContext);
+  return (
+    <div className="min-h-screen bg-gray-800">
+      {children}
+      {loading ? <ModalLoader /> : null}
+    </div>
+  );
+};
+
+export default Layout;
