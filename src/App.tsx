422b4c1f1e35975014415c7b35e0cd2d32f82cf2
fmaulll <maul2821@gmail.com>
2023-06-20 22:19:53 +0700

    removing detail page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout";
 import Home from "./pages/Home";
 import PokemonProvider from "./context/PokemonContext";
-import Detail from "./pages/Detail";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/detail/:id" element={<Detail />} />
           </Routes>
         </Layout>
       </PokemonProvider>
