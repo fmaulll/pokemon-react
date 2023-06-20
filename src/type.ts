606dbab0c4e6cec4c45cde389643a8a613150bd5
fmaulll <maul2821@gmail.com>
2023-06-20 16:47:35 +0700

    pokemon types

---
+++ src/type.ts
@@ -0,0 +1,18 @@
+export type PokemonTypes = {
+  id: number;
+  name: string;
+  image: string;
+  weight: number;
+  height: number;
+  types: string;
+};
+
+export type PokemonTypeList = {
+  name: string;
+  url: string;
+};
+
+export type FilterTypes = {
+  name: string;
+  url: string;
+};
