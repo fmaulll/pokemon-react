d835b0297d89101c536d65fc50dde2c6575e6314
fmaulll <maul2821@gmail.com>
2023-06-22 09:48:52 +0700

    fix filter error in test

--- src/components/Filter/__test__/Filter.test.tsx
+++ src/components/Filter/__test__/Filter.test.tsx
@@ -4,7 +4,7 @@ import Filter from "..";
 
 describe("<Filter/>", () => {
   it("should display the filter component properly", () => {
-    const { getByTestId } = render(<Filter onResetFilter={() => {}} onSubmitFilter={() => {}} typeList={[]} />);
+    const { getByTestId } = render(<Filter total={0} onResetFilter={() => {}} onSubmitFilter={() => {}} typeList={[]} />);
 
     expect(getByTestId("filter")).toBeInTheDocument();
   });
